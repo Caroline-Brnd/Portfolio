9b6a448f685be020ac9bf4825d0f5cada72448e2
Caroline-Brnd <caronj6.11@gmail.com>
2024-09-19 12:46:32 +0200

    update projets

--- js/script.js
+++ js/script.js
@@ -2,3 +2,19 @@ function toggleMenu() {
     const navbar = document.querySelector('.navbar');
     navbar.classList.toggle('active');
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const competencesSection = document.querySelector('.competences-icon');
+
+    // Fonction pour vérifier la position de l'élément
+    function handleScroll() {
+        const sectionPosition = competencesSection.getBoundingClientRect().top;
+        const screenPosition = window.innerHeight / 1.3;
+
+        if (sectionPosition < screenPosition) {
+            competencesSection.classList.add('show'); // Ajouter la classe 'show' pour déclencher l'animation
+        }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+});
