f8327837db605b37dcdcb4d5b480a90fad25ce3d
Caroline-Brnd <caronj6.11@gmail.com>
2024-09-24 11:43:18 +0200

    update contact + hobby

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
+// Burger
 function toggleMenu() {
     const navbar = document.querySelector('.navbar');
     navbar.classList.toggle('active');
 }
 
+// Animation Compétences
 document.addEventListener('DOMContentLoaded', function() {
     const competencesSection = document.querySelector('.competences-icon');
 
@@ -19,6 +21,32 @@ document.addEventListener('DOMContentLoaded', function() {
     window.addEventListener('scroll', handleScroll);
 });
 
+// Date conforme de MAJ
 const lastModified = document.lastModified;
 const formattedDate = moment(lastModified).locale('fr').format('DD/MM/YYYY à HH:mm'); // Formatage date
 document.getElementById("last-modified").textContent = formattedDate;
+
+// Validation email
+const emailInput = document.getElementById('email');
+const emailValidation = document.getElementById('email-valid');
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+emailInput.addEventListener('input', () => {
+  if (emailRegex.test(emailInput.value)) {
+    emailValidation.textContent = 'Adresse email valide';
+    emailValidation.style.color = 'green';
+  } else {
+    emailValidation.textContent = 'Adresse email invalide';
+    emailValidation.style.color = 'red';
+  }
+});
+
+// Bouton retour en haut
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    })
+});
